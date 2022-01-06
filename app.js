60a7a20dae46472ccfc4b5bf43a97eb936c99741
Pranat <pranat@lucknowmarbles.com>
2022-01-06 17:08:13 +0530

    sixthCommit

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const graphqlHTTP = require('express-graphql').graphqlHTTP;
 const schema = require('./schema/schema');
 const mongoose = require('mongoose');
 const cors = require('cors');
-const PORT = process.env.port || 4000;
+const PORT = process.env.PORT || 4000;
 
 const app = express();
 
